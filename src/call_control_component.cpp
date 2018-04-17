f4f916d89116de647ab060ae078aacf1836a6010
Julien Chavanton <jchavanton@gmail.com>
2018-04-17 08:15:15 -0700

    kamailio RPC URL

--- src/call_control_component.cpp
+++ src/call_control_component.cpp
@@ -11,25 +11,28 @@ using namespace std;
 #include <xmlrpc-c/server_abyss.hpp>
 #include <xmlrpc-c/client_simple.hpp>
 
-int kam_dlg_dlg_list(string const &callId) {
+int kam_dlg_dlg_list(string const kamXmlRpc, string const &callId) {
 	try {
-		string const serverUrl("http://147.75.69.33:4291/RPC2");
 		string const methodName("dlg.dlg_list");
 		xmlrpc_c::clientSimple myClient;
 		xmlrpc_c::value result;
-
 		xmlrpc_c::paramList paramList;
 		xmlrpc_c::value_string callIdParam(callId);
 		paramList.add(callIdParam);
 
-		myClient.call(serverUrl, methodName, paramList, &result);
+		myClient.call(kamXmlRpc, methodName, paramList, &result);
 		xmlrpc_c::value_struct call_struct(result);
 		map<string, xmlrpc_c::value> call_cstruct(call_struct);
 		string callId = xmlrpc_c::value_string(call_cstruct["call-id"]);
+		// callee info
 		xmlrpc_c::value_struct callee_struct(call_cstruct["callee"]);
 		map<string, xmlrpc_c::value> callee_cstruct(callee_struct);
 		string totag = xmlrpc_c::value_string(callee_cstruct["tag"]);
-		cout << "call["<< callId <<"] totag["<< totag <<"]"<< endl;
+		// caller info
+		xmlrpc_c::value_struct caller_struct = xmlrpc_c::value_struct(call_cstruct["caller"]);
+		std::map<std::string, xmlrpc_c::value> caller_cstruct(caller_struct);
+		string fromtag = xmlrpc_c::value_string(caller_cstruct["tag"]);
+		cout << "call["<< callId <<"]fromtag["<< fromtag <<"]totag["<< totag <<"]"<< endl;
 
 	} catch (exception const& e) {
 		cerr << "Client threw error: " << e.what() << endl;
@@ -38,13 +41,12 @@ int kam_dlg_dlg_list(string const &callId) {
 	}
 }
 
-int kam_dlg_list() {
+int kam_dlg_list(string const kamXmlRpc) {
 	try {
-		string const serverUrl("http://147.75.69.33:4291/RPC2");
 		string const methodName("dlg.list");
 		xmlrpc_c::clientSimple myClient;
 		xmlrpc_c::value result;
-		myClient.call(serverUrl, methodName, &result);
+		myClient.call(kamXmlRpc, methodName, &result);
 		xmlrpc_c::value_array arrayData(result);
 		cout << "calls : " << arrayData.size() << endl;
 		vector<xmlrpc_c::value> callValues = arrayData.vectorValueValue();
@@ -55,10 +57,9 @@ int kam_dlg_list() {
 				map<string, xmlrpc_c::value> call_cstruct(call_struct);
 				string callId = xmlrpc_c::value_string(call_cstruct["call-id"]);
 				// cout << "call["<< callId <<"]" << endl;
-				kam_dlg_dlg_list(callId);
+				kam_dlg_dlg_list(kamXmlRpc, callId);
 			}
 			callValues.pop_back();
-
 		}
 	} catch (exception const& e) {
 		cerr << "Client threw error: " << e.what() << endl;
@@ -69,21 +70,24 @@ int kam_dlg_list() {
 
 class MethodDlgTerminate : public xmlrpc_c::method {
 public:
-	MethodDlgTerminate() {
-		this->_signature = "i:sss";
+	MethodDlgTerminate(string const kamXmlRpc) {
+		this->_kamXmlRpc = kamXmlRpc;
+		this->_signature = "i:ss";
 		// method's result is int and 3 arguments are strings
 		this->_help = "This method terminate a call";
 	}
 	void execute(xmlrpc_c::paramList const& paramList, xmlrpc_c::value * const retvalP) {
 		string const callid(paramList.getString(0));
-		//kam_dlg_dlg_list(callid);
+		// kam_dlg_dlg_list(callid);
 		string const fromtag(paramList.getString(1));
-		string const totag(paramList.getString(2));
+		// string const totag(paramList.getString(2));
 		paramList.verifyEnd(3);
 		*retvalP = xmlrpc_c::value_int(1);
-		kam_dlg_list();
+		kam_dlg_list(_kamXmlRpc);
 		// Disconnect the call on Kamailio
 	}
+private:
+	string _kamXmlRpc;
 };
 
 int main(int const argc, const char *argv[]) {
@@ -92,11 +96,15 @@ int main(int const argc, const char *argv[]) {
 	if (argc > 1) port = atoi(argv[1]);
 	string abyssLogFileName = "/tmp/abyss.log";
 	if (argc > 2) abyssLogFileName.assign(argv[2]);
-	kam_dlg_list();
+	string kamXmlRpc = "http://";
+	if (argc > 3) kamXmlRpc = kamXmlRpc + argv[3] + "/RPC2";
+	else kamXmlRpc = kamXmlRpc + "127.0.0.1:4291/RPC2";
+
+	kam_dlg_list(kamXmlRpc);
 	try {
 		xmlrpc_c::registry myRegistry;
-		xmlrpc_c::methodPtr const MethodDlgTerminateP(new MethodDlgTerminate);
-		myRegistry.addMethod("dlg.terminate_dlg", MethodDlgTerminateP);
+		xmlrpc_c::methodPtr const MethodDlgTerminateP(new MethodDlgTerminate(kamXmlRpc));
+		myRegistry.addMethod("mi", MethodDlgTerminateP);
 
 		xmlrpc_c::serverAbyss myAbyssServer(
 			xmlrpc_c::serverAbyss::constrOpt()
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./call_control_component 8080 "/var/log/ccc.log" "147.75.69.33:4291"
