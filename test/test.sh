43d03347ebd9bd9d197200539d49c9eac2efbdaa
Julien Chavanton <jchavanton@gmail.com>
2018-04-17 15:27:58 -0700

    adding test script

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-cat test/test_mi.txt | nc 127.0.0.1 8080
-cat test/test_message.txt | nc 127.0.0.1 8080
+
+INSTRUCTIONS="$0 <call-id>\n";
+
+if [ "$1" == "" ]
+then
+	printf "${INSTRUCTIONS}"
+	exit
+fi
+
+
+CALL_ID=$1
+LEN=${#CALL_ID}
+CONTENT_LEN=$((283+${LEN}))
+printf "call_id[${CALL_ID}]len[${LEN}][$CONTENT_LEN]\n"
+
+cat test/test_mi.txt | sed "s/CALL_ID/${CALL_ID}/ ; s/CONTENT_LEN/${CONTENT_LEN}/" > /tmp/test_mi.txt
+cat /tmp/test_mi.txt | nc 127.0.0.1 8080
